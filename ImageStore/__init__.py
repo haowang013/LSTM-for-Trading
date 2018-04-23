21ebf3fb8e7dad522fdf5da91b72552d437bdd8c
richardwang013 <30414142+richardwang013@users.noreply.github.com>
2018-04-23 23:51:09 +0800

    Create __init__.py

---
+++ ImageStore/__init__.py
@@ -0,0 +1,13 @@
+import numpy as np
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+def init():
+  print('##############')
+  print('It's a initial script.')
+  print('##############')
+  
+if __main__ == '__main__':
+  init()
